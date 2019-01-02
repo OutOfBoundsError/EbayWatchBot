5c03f6dc10929b78de03ccd5d52b988ebab53758
Johnson <johnsonnguy1999@hotmail.com>
2019-01-02 15:00:41 -0800

    Update ebayWatch.py

--- ebayWatch.py
+++ ebayWatch.py
@@ -14,7 +14,6 @@ import time
 def createSession():
     chrome_option = Options()
     chrome_option.add_argument("--headless")
-    #chrome_option.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36")
     driver = webdriver.Chrome()
     return driver
 
